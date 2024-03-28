b0fe3e80decbaaea979c1a40144398a962263671
Alexandr Dementev <ad@adictgroup.ru>
2024-03-28 09:35:59 +0800

    config project

---
+++ src/app/App.jsx
@@ -0,0 +1,12 @@
+import reactLogo from "../assets/react.svg";
+
+function App() {
+    return (
+        <>
+            <img src={reactLogo} className="logo react" alt="React logo" />
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </>
+    );
+}
+
+export default App;
